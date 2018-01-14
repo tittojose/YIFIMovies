4487fe25777771f0587b4bbbe67997390dbe3e28
Titto Jose <mailtitto@gmail.com>
2018-01-14 17:06:55 +0530

    Search UI implemetation

--- app/src/main/java/yifimovies/tittojose/me/yifi/HomeActivity.java
+++ app/src/main/java/yifimovies/tittojose/me/yifi/HomeActivity.java
@@ -1,5 +1,7 @@
 package yifimovies.tittojose.me.yifi;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -7,7 +9,11 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +21,7 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     @BindView(R.id.tabs)
     TabLayout tabLayout;
@@ -42,6 +48,19 @@ public class HomeActivity extends AppCompatActivity {
         adapter.addFragment(new TopMoviesFragment(), "Top");
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        Toast.makeText(HomeActivity.this, "Submitted - " + query, Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        Toast.makeText(HomeActivity.this, newText, Toast.LENGTH_SHORT).show();
+        //use the query to search your data somehow
+        return false;
     }
 
     private class ViewPagerAdapter extends FragmentPagerAdapter {
@@ -71,5 +90,25 @@ public class HomeActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             return mFragmentTitleList.get(position);
         }
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+
+        searchView.setOnQueryTextListener(this);
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
     }
 }
---
+++ app/src/main/java/yifimovies/tittojose/me/yifi/SearchResultsActivity.java
@@ -0,0 +1,35 @@
+package yifimovies.tittojose.me.yifi;
+
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+/**
+ * Created by titto.jose on 14-01-2018.
+ */
+
+public class SearchResultsActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(SearchResultsActivity.this, query, Toast.LENGTH_SHORT).show();
+            //use the query to search your data somehow
+        }
+    }
+}
