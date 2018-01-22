a8fbc420faad0eef6ca6be41972f0814d1371f19
Titto Jose <mailtitto@gmail.com>
2018-01-22 21:21:57 +0530

    Rate and share app feature.

--- app/src/main/java/yifimovies/tittojose/me/yifi/homescreen/HomeActivity.java
+++ app/src/main/java/yifimovies/tittojose/me/yifi/homescreen/HomeActivity.java
@@ -1,11 +1,16 @@
 package yifimovies.tittojose.me.yifi.homescreen;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -39,6 +44,12 @@ public class HomeActivity extends AppCompatActivity {
     @BindView(R.id.coordinatorLayoutHome)
     CoordinatorLayout parentCoordinatorLayout;
 
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
+    @BindView(R.id.navigation_view)
+    NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +57,12 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        setupNavigationDrawer();
         setupViewPager(viewPager);
         setupSuggestionsRecyclerView();
 
@@ -54,6 +71,51 @@ public class HomeActivity extends AppCompatActivity {
                     .make(parentCoordinatorLayout, "No Internet connection available. Please check your network connectivity and try again.", Snackbar.LENGTH_LONG);
 
             snackbar.show();
+        }
+    }
+
+    private void setupNavigationDrawer() {
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.drawer_rate_app:
+                        startRateAppIntent();
+                        break;
+                    case R.id.drawer_share_app:
+                        startShareAppIntent();
+                        break;
+                }
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+
+    private void startShareAppIntent() {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_SUBJECT, "My application name");
+        sendIntent.putExtra(Intent.EXTRA_TEXT,
+                getString(R.string.shared_via));
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
+    }
+
+    private void startRateAppIntent() {
+        Uri uri = Uri.parse("market://details?id=" + getPackageName());
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+        // To count with Play market backstack, After pressing back button,
+        // to taken back to our application, we need to add following flags to intent.
+        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        try {
+            startActivity(goToMarket);
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName())));
         }
     }
 
@@ -80,6 +142,7 @@ public class HomeActivity extends AppCompatActivity {
         return true;
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -88,6 +151,10 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(new Intent(HomeActivity.this, SearchSuggestionActivity.class));
                 overridePendingTransition(0, 0);
                 break;
+
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
