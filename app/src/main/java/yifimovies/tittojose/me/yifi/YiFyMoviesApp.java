f0d6e84f95676316f4841f4edb401409c00a06c0
Titto Jose <mailtitto@gmail.com>
2018-03-22 22:10:51 +0530

    Remove crashlytics integration.

--- app/src/main/java/yifimovies/tittojose/me/yifi/YiFyMoviesApp.java
+++ app/src/main/java/yifimovies/tittojose/me/yifi/YiFyMoviesApp.java
@@ -3,8 +3,6 @@ package yifimovies.tittojose.me.yifi;
 import android.app.Application;
 
 import com.onesignal.OneSignal;
-import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by titto.jose on 20-02-2018.
@@ -14,7 +12,6 @@ public class YiFyMoviesApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Fabric.with(this, new Crashlytics());
         OneSignal.startInit(this)
                 .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
                 .unsubscribeWhenNotificationsAreDisabled(true)
